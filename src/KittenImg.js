cd3766e65223c686df39c770f2913cb7071833d1
Anna Zocher <anna.zocher@gmail.com>
2021-02-01 12:57:33 -0800

    Supersonics in class intro to class based components and React

---
+++ src/KittenImg.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class KittenImg extends Component {
+    render() {
+        return (
+            <div>
+                <img src="http://www.placekitten.com/200/200" />
+            </div>
+        )
+    }
+}
+
+export default KittenImg;
